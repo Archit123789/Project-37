e5c0f729c95e398a16f23f5b2ad407bb1cecc552
Archit123789 <79358995+Archit123789@users.noreply.github.com>
2021-05-03 12:59:34 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,63 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    
+    Question.input1.hide();
+    Question.button.hide();
+    Question.input2.hide();
+    //write code here to hide question elements
+    
+    //write code to change the background color here
+    background("cyan")
+    //write code to show a heading for showing the result of Quiz
+    textSize(35)
+    text("Correct",350,230)
+    //call getContestantInfo( ) here
+    Contestant.getPlayerInfo()
+    //write condition to check if contestantInfor is not undefined
+    if(allContestants !== undefined){
+    fill("blue")
+    textSize(20);
+    text("*NOTE: Contestant who answered correct will be highlighted in green color",130,230)
+    }
+  
+    //write code to highlight contest who answered correctly
+    for(var plr in allContestants ){
+      var correctAns = "2"
+      if (correctAns === allContestants[plr].answer)
+      fill("Green")
+      else
+      fill("Red")
+    }
+    
+  }
+
+}
